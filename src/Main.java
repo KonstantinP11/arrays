b6c924e80f8cef0e43dffe921da5ca12cd497312
conditional <105pkn@mail.ru>
2023-05-22 22:04:07 +0700

    init commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+
+        task1();
+ /*       task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();*/
+    }
+
+    public static void task1() {
+        System.out.println("задача 1");
+
+        int[] weights = {
+                103, 102, 101, 100, 99, 100, 98, 0
+        };
+        //      weights[0] = 90;
+        int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        //      System.out.println(weights[13]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+
+        }
+
+    }
+}
